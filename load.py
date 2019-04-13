04b4da2f84b21c12ce11c7c0040eee740d3e2e29
Mohamed Sayed <ms10596@gmail.com>
2019-04-13 19:24:21 +0200

    feat: logistic regression model with 0.86 starting accuracy on 100 random test samples

--- load.py
+++ load.py
@@ -1,6 +1,10 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
 import glob
-import numpy as np
+from random import shuffle
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
 
 def ViewFiles():
     list_of_Negfiles = glob.glob('./corpus/neg/*.txt')
@@ -15,19 +19,38 @@ def ViewFiles():
         f = open(i)
         samples.append(f.read())
         labels.append(1)
+    samples_labels = list(zip(samples, labels))
+    shuffle(samples_labels)
+    samples, labels = zip(*samples_labels)
     return samples, labels
     # print("Negatives: ", list_of_Negfiles)
     # print("Positives: ", list_of_Posfiles)
+
+
 def tf_idf(samples):
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(samples)
     return X
+
+def model(samples, labels):
+    clf = LogisticRegression()
+    clf.fit(samples[:1900], labels[:1900])
+    print(clf.score(samples[1900:], labels[1900:]))
+    return clf
+
+
+
+
 def main():
     samples, labels = ViewFiles()
+    # print(labels)
     # for i in samples:
     #     print(i)
     #     print()
-    print(tf_idf(samples)[0])
+    # print(tf_idf(samples)[0])
+
+    model(tf_idf(samples), labels)
+
 
 if __name__ == "__main__":
     main()
