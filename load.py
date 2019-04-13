d61c1502ca1a04fc7112255b5880901ce5276d6d
Mohamed Sayed <ms10596@gmail.com>
2019-04-13 18:25:35 +0200

    feat: load texts and labels in one list. Please update project before editing anything.

--- load.py
+++ load.py
@@ -4,12 +4,21 @@ import glob
 def ViewFiles():
     list_of_Negfiles = glob.glob('./corpus/neg/*.txt')
     list_of_Posfiles = glob.glob('./corpus/pos/*.txt')
-    print("Negatives: ", list_of_Negfiles)
-    print("Positives: ", list_of_Posfiles)
+    texts = []
+    for i in list_of_Negfiles:
+        f = open(i)
+        texts.append((f.read(), 0))
+    for i in list_of_Posfiles:
+        f = open(i)
+        texts.append((f.read(), 1))
+    return texts
+    # print("Negatives: ", list_of_Negfiles)
+    # print("Positives: ", list_of_Posfiles)
+    # return list_of_Posfiles,list_of_Negfiles
 
 
 def main():
-    ViewFiles()
+    print(ViewFiles()[999][1])
 
 if __name__ == "__main__":
     main()
