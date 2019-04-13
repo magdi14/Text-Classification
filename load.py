d7341d7d9855dcd0136694533d09a10479c01fb2
Mohamed Sayed <ms10596@gmail.com>
2019-04-13 18:30:26 +0200

    refactor: list for samples and list for labels

--- load.py
+++ load.py
@@ -4,17 +4,19 @@ import glob
 def ViewFiles():
     list_of_Negfiles = glob.glob('./corpus/neg/*.txt')
     list_of_Posfiles = glob.glob('./corpus/pos/*.txt')
-    texts = []
+    samples = []
+    labels = []
     for i in list_of_Negfiles:
         f = open(i)
-        texts.append((f.read(), 0))
+        samples.append(f.read())
+        labels.append(0)
     for i in list_of_Posfiles:
         f = open(i)
-        texts.append((f.read(), 1))
-    return texts
+        samples.append(f.read())
+        labels.append(1)
+    return samples, labels
     # print("Negatives: ", list_of_Negfiles)
     # print("Positives: ", list_of_Posfiles)
-    # return list_of_Posfiles,list_of_Negfiles
 
 
 def main():
