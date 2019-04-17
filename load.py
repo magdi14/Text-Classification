ffc99afcb344e213024c136eb2d2f57119108510
Mohamed Sayed <ms10596@gmail.com>
2019-04-17 21:37:36 +0200

    refactor: rename functions

--- load.py
+++ load.py
@@ -3,22 +3,21 @@ from random import shuffle, seed
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 
-def ViewFiles():
-    list_of_Negfiles = glob.glob('./corpus/neg/*.txt')
-    list_of_Posfiles = glob.glob('./corpus/pos/*.txt')
+def load_data():
+    list_of_negfiles = glob.glob('./corpus/neg/*.txt')
+    list_of_posfiles = glob.glob('./corpus/pos/*.txt')
     samples = []
     labels = []
-    for i in list_of_Negfiles:
+    for i in list_of_negfiles:
         f = open(i)
         samples.append(f.read())
         labels.append(0)
-    for i in list_of_Posfiles:
+    for i in list_of_posfiles:
         f = open(i)
         samples.append(f.read())
         labels.append(1)
@@ -32,9 +31,12 @@ def model(samples, labels):
     clf = LogisticRegression(solver='lbfgs')
     clf.fit(samples[:1800], labels[:1800])
     print("Accuracy:", clf.score(samples[1800:], labels[1800:]))
+    return clf
+
+
+def plot(samples, labels):
     fig = plt.figure()
     ax = Axes3D(fig)
-
     data2d = TruncatedSVD(n_components=3).fit_transform(samples)
     print(data2d.shape)
     for i in range(len(labels[1800:])):
@@ -43,7 +45,6 @@ def model(samples, labels):
         else:
             ax.scatter(data2d[:, 0][i], data2d[:, 1][i], data2d[:, 2][i], c='red')
     plt.show()
-    return clf
 
 
 def predict_from_file(vectorizer, clf):
@@ -55,8 +56,9 @@ def predict_from_file(vectorizer, clf):
 
 if __name__ == "__main__":
     seed(2)
-    samples, labels = ViewFiles()
+    samples, labels = load_data()
     vectorizer = TfidfVectorizer(stop_words='english')
     X = vectorizer.fit_transform(samples)
     clf = model(X, labels)
+    plot(X, labels)
     # print(predict_from_file(vectorizer, clf))
