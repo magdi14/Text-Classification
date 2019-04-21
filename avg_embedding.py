7864de6a66c3dd7b521e29818aea0efb833a8b1a
Mohamed Sayed <ms10596@gmail.com>
2019-04-21 16:02:58 +0200

    fix: avg was working in wrong way

--- avg_embedding.py
+++ avg_embedding.py
@@ -7,7 +7,7 @@ from sum_embedding import SumEmbeddingsModel
 class AvgEmbeddingsModel(SumEmbeddingsModel):
     def transform(self, samples, labels):
         x = np.array(
-            [np.sum(np.array([self.word_embeddings[j] for j in word_tokenize(i)], dtype=np.float), axis=0) for i in
+            [np.average(np.array([self.word_embeddings[j] for j in word_tokenize(i)], dtype=np.float), axis=0) for i in
              samples])
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, labels, random_state=1,
                                                                                 test_size=0.2)
