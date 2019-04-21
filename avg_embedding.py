4dc33f0c0d3999ae1288daa26735ea389450274b
Mohamed Sayed <ms10596@gmail.com>
2019-04-21 15:36:02 +0200

    feat: avg_embedding_model

--- avg_embedding.py
+++ avg_embedding.py
@@ -1,2 +1,19 @@
-import gensim
+import numpy as np
+from nltk import word_tokenize
+from sklearn.model_selection import train_test_split
+from utils.loading import load_data, load_glove_embeddings
+from sum_embedding import SumEmbeddingsModel
 
+
+class AvgEmbeddingsModel(SumEmbeddingsModel):
+    def transform(self, samples, labels):
+        x = np.array(
+            [np.sum(np.array([self.word_embeddings[j] for j in word_tokenize(i)], dtype=np.float), axis=0) for i in
+             samples])
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, labels, random_state=1,
+                                                                                test_size=0.2)
+
+
+if __name__ == '__main__':
+    samples, labels = load_data()
+    t = AvgEmbeddingsModel(samples, labels, load_glove_embeddings())
--- sum_embedding.py
+++ sum_embedding.py
@@ -1,10 +1,11 @@
+import numpy as np
 from nltk import word_tokenize
+from sklearn.model_selection import train_test_split
 
 from tfidf import TfidfModel
-from utils.plotting import plot
-from sklearn.model_selection import train_test_split
-import numpy as np
 from utils.loading import load_glove_embeddings, load_data
+from utils.plotting import plot
+
 
 class SumEmbeddingsModel(TfidfModel):
     def __init__(self, samples, labels, word_embeddings):
@@ -14,16 +15,13 @@ class SumEmbeddingsModel(TfidfModel):
         plot(self.x_test, self.y_test)
 
     def transform(self, samples, labels):
-        x = np.array([np.sum(np.array([self.word_embeddings[j] for j in word_tokenize(i)], dtype=np.float),axis=0) for i in samples])
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x,labels, random_state=1, test_size=0.2)
-
-
+        x = np.array(
+            [np.sum(np.array([self.word_embeddings[j] for j in word_tokenize(i)], dtype=np.float), axis=0) for i in
+             samples])
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, labels, random_state=1,
+                                                                                test_size=0.2)
 
 
 if __name__ == '__main__':
     samples, labels = load_data()
     t = SumEmbeddingsModel(samples, labels, load_glove_embeddings())
-
-
-
-
